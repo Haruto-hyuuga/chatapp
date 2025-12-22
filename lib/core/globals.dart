01e6df818f14b4ecf3526b0ae0258d76b49e95c3
Hinata-hyuuga <maidevshiro@gmail.com>
2025-12-22 22:17:59 +0530

    stupid mistake

--- lib/core/globals.dart
+++ lib/core/globals.dart
@@ -1,1 +1,2 @@
-const String backendApiBaseUrl = "https://chatapp-backend-1b8y.onrender.com/";
+// DO NOT PUT '/' AT END OF URL WE DONT WANT baseurl//auth/ IT WILL CRASH
+const String backendApiBaseUrl = "https://chatapp-backend-1b8y.onrender.com";
